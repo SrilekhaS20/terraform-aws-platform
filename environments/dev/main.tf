ce8be6228c497f160c5c2974cd9155f5a070ce5a
SrilekhaS20 <lekhaconst.1@gmail.com>
2026-09-17 10:11:37 +0530

    Added Public Route Table

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -26,3 +26,16 @@ resource "aws_internet_gateway" "igw-main" {
     Name = "${var.environment}-igw"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-main.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
