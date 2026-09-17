2263e95fb0dfc6e37d6eea519276cd48ec57f76d
SrilekhaS20 <lekhaconst.1@gmail.com>
2026-09-17 10:47:06 +0530

    Add Public Route Table Association

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_sub" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
@@ -19,7 +19,7 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_internet_gateway" "igw-main" {
+resource "aws_internet_gateway" "igw_main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -32,10 +32,15 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw-main.id
+    gateway_id = aws_internet_gateway.igw_main.id
   }
 
   tags = {
     Name = "${var.environment}-public-rt"
   }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_sub.id
+  route_table_id = aws_route_table.public_rt.id
 }
--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -5,5 +5,5 @@ output "vpc_id" {
 
 output "public_subnet_id" {
   description = "ID of the public subnet"
-  value       = aws_subnet.public.id
+  value       = aws_subnet.public_sub.id
 }
