9b97f32e92c3ed7ffd17b4489f04ab094424897c
SrilekhaS20 <lekhaconst.1@gmail.com>
2026-09-17 09:58:23 +0530

    Add public subnet

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main.id
 }
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
